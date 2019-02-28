8475501bb47a2c31513ee4d5bc2772e00768ff3c
Stykers <stykers@syskiller.net>
2019-02-28 14:02:13 +0900

    me derping be like

---
+++ util/list.py
@@ -0,0 +1,5 @@
+ballresponse = [
+  'Yes', 'No', 'Take a wild guess...', 'Very doubtful',
+  'Sure', 'Without a doubt', 'Most likely', 'Might be possible',
+  "Segmentation fault.", 'no... (╯°□°）╯︵ ┻━┻', 'senpai, pls no ;-;'
+]
