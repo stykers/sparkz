9827a578591ec11bf440f4d2682b6ec69f150218
Stykers <stykers@syskiller.net>
2019-02-28 13:08:41 +0900

    LMAO

--- util/permissions.py
+++ util/permissions.py
@@ -10,3 +10,32 @@ async def check_permissions(ctx, perms, *, check=all):
 
     resolved = ctx.channel.permissions_for(ctx.author)
     return check(getattr(resolved, name, None) == value for name, value in perms.items())
+
+
+def has_permissions(*, check=all, **perms):
+    async def potato(ctx):
+        return await check_permissions(ctx, perms, check=check)
+    return commands.check(potato)
+
+
+def can_send(ctx):
+    return isinstance(ctx.channel, discord.DMChannel) or ctx.channel.permissions_for(ctx.guild.me).send_messages
+
+
+def can_embed(ctx):
+    return isinstance(ctx.channel, discord.DMChannel) or ctx.channel.permissions_for(ctx.guild.me).embed_links
+
+
+def can_upload(ctx):
+    return isinstance(ctx.channel, discord.DMChannel) or ctx.channel.permissions_for(ctx.guild.me).attach_files
+
+
+def can_react(ctx):
+    return isinstance(ctx.channel, discord.DMChannel) or ctx.channel.permissions_for(ctx.guild.me).add_reactions
+
+
+def is_nsfw(ctx, locale):
+    if locale is not "ja_JP":
+        return isinstance(ctx.channel, discord.DMChannel) or ctx.channel.is_nsfw()
+    else:
+        return True
