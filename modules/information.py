7bc665c9f324bfe588e53056c8acf5b750cd91eb
Stykers <stykers@syskiller.net>
2019-02-28 22:43:59 +0900

    userinfo as everyone have wanted

--- modules/information.py
+++ modules/information.py
@@ -47,6 +47,29 @@ class Information(commands.Cog):
 
         await ctx.send(content=f"ℹ About **{ctx.bot.user}** | **{repository.version}**", embed=embed)
 
+    @commands.command()
+    @commands.guild_only()
+    async def user(self, ctx, *, user: discord.Member = None):
+        """ Retrieve user info. """
+        if user is None:
+            user = ctx.author
+
+        embed = discord.Embed(colour=user.top_role.colour.value)
+        embed.set_thumbnail(url=user.avatar_url)
+
+        embed.add_field(name="User Name", value=user, inline=True)
+        embed.add_field(name="Guild Name", value=user.nick if hasattr(user, "nick") else "None", inline=True)
+        embed.add_field(name="Register Date", value=essential.date(user.created_at), inline=True)
+        embed.add_field(name="Join Date", value=essential.date(user.joined_at), inline=True)
+
+        embed.add_field(
+            name="Roles",
+            value=', '.join([f"<@&{x.id}>" for x in user.roles if x is not ctx.guild.default_role]) if len(user.roles) > 1 else 'None',
+            inline=False
+        )
+
+        await ctx.send(content=f"ℹ About **{user.id}**", embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Information(bot))
