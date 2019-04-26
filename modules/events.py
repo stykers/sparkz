27f3f23405e6266e9f5cae88ddd2e97cb80f1e2e
Stykers <stykers@stykers.moe>
2019-04-26 11:50:54 +0900

    event related stuff

---
+++ modules/events.py
@@ -0,0 +1,44 @@
+import discord
+import traceback
+import psutil
+import os
+
+from datetime import datetime
+from discord.ext import commands
+from discord.ext.commands import errors
+from util import essential
+
+
+async def send_command_help (ctx):
+    if ctx.invoked_subcommand:
+        _help = await ctx.bot.formatter.format_help_for(ctx, ctx.invoked_subcommand)
+    else:
+        _help = await ctx.bot.formatter.format_help_for(ctx, ctx.command)
+
+    for page in _help:
+        await ctx.send(page)
+
+
+class Events(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.config = essential.get("config.json")
+        self.process = psutil.Process(os.getpid())
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, err):
+        if isinstance(err, errors.MissingRequiredArgument) or isinstance(err, errors.BadArgument):
+            await send_command_help(ctx)
+
+        elif isinstance(err, errors.CommandInvokeError):
+            err = err.original
+
+            _traceback = traceback.format_tb(err.__traceback__)
+            _traceback = ''.join(_traceback)
+            error = ('```py\n{2}{0}: {3}\n```').format(type(err).__name__, ctx.message.content, _traceback, err)
+
+            await ctx.send(f"An error occurred while the server is interpreting your command.")
+
+
+def setup(bot):
+    bot.add_cog(Events(bot))
