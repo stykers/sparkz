038b4353d8493a2b5e4cc7d287d764da3842fe7a
Stykers <stykers@stykers.moe>
2019-05-23 19:46:15 +0900

    slot, you maybe unable to see it in the code but it will most definitely work

--- modules/fun.py
+++ modules/fun.py
@@ -115,6 +115,24 @@ class Fun(commands.Cog):
             bio = BytesIO(fin.read())
         await ctx.send(file=discord.File(bio, filename="noticeme.gif"))
 
+    @commands.command(aliases=['slots', 'bet'])
+    @commands.cooldown(rate=1, per=3.0, type=commands.BucketType.user)
+    async def slot(self, ctx):
+        """ Roll the slot machine """
+        emojis = "🍎🍊🍐🍋🍉🍇🍓🍒"
+        a = random.choice(emojis)
+        b = random.choice(emojis)
+        c = random.choice(emojis)
+
+        slotmachine = f"**[ {a} {b} {c} ]\n{ctx.author.name}**,"
+
+        if a == b == c:
+            await ctx.send(f"{slotmachine} All matching, you won! 🎉")
+        elif (a == b) or (a == c) or (b == c):
+            await ctx.send(f"{slotmachine} 2 in a row, you won! 🎉")
+        else:
+            await ctx.send(f"{slotmachine} No match, you lost 😢")
+
 
 def setup(bot):
     bot.add_cog(Fun(bot))
