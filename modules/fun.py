2527c2cb630d0906074d3db1332a4498f983d574
Stykers <stykers@stykers.moe>
2019-05-23 14:37:58 +0900

    placed a bunch of stuff into one module

---
+++ modules/fun.py
@@ -0,0 +1,64 @@
+import random
+import discord
+import json
+import secrets
+import aiohttp
+
+from io import BytesIO
+from discord.ext import commands
+from util import list, permissions, http, essential
+
+
+class Fun(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def hug(self, context, member: discord.Member):
+        """Hug your senpai/waifu!"""
+        author = context.message.author.mention
+        mention = member.mention
+
+        hug = "**{0} gave {1} a hug!**"
+
+        choices = ['https://cdn.stykers.moe/img/hug/1.gif', 'https://cdn.stykers.moe/img/hug/2.gif', 'https://cdn.stykers.moe/img/hug/3.gif',
+                   'https://cdn.stykers.moe/img/hug/4.gif', 'https://cdn.stykers.moe/img/hug/5.gif']
+
+        image = random.choice(choices)
+
+        embed = discord.Embed(colour=discord.Colour.blue(), description=hug.format(author, mention))
+        embed.set_image(url=image)
+
+        await context.send(embed=embed)
+
+    @commands.command(pass_context=True)
+    async def kiss(self, context, member: discord.Member):
+        """Kiss your senpai/waifu!"""
+        author = context.message.author.mention
+        mention = member.mention
+
+        kiss = "**{0} gave {1} a kiss!**"
+
+        choices = ['https://cdn.stykers.moe/img/kiss/1.gif', 'https://cdn.stykers.moe/img/kiss/2.gif', 'https://cdn.stykers.moe/img/kiss/3.gif',
+                   'https://cdn.stykers.moe/img/kiss/4.gif', 'https://cdn.stykers.moe/img/kiss/5.gif']
+
+        image = random.choice(choices)
+
+        embed = discord.Embed(colour=discord.Colour.blue(), description=kiss.format(author, mention))
+        embed.set_image(url=image)
+
+        await context.send(embed=embed)
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def neko(self, ctx):
+        """Nekos! \\o/ Warning: Some lewd nekos exist :eyes:"""
+        async with self.session.get("https://nekos.life/api/neko") as resp:
+            nekos = await resp.json()
+
+        embed = discord.Embed(colour=discord.Colour.blue())
+        embed.set_image(url=nekos['neko'])
+        await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Fun(bot))
