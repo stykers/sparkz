dc573ee6ac240852e2aa25483e6af811d0e913d9
Stykers <stykers@stykers.moe>
2019-05-24 16:18:44 +0900

    just the search thing and not found to go

--- modules/help.py
+++ modules/help.py
@@ -13,18 +13,24 @@ class Help(commands.Cog):
         """The help command."""
         if not module:
             content = discord.Embed(title='Overall Help',
-                                    description='Use `s!help <module>` to view help of a specific module')
+                                    description='Use `s!help <module>` to view help of a specific module',
+                                    colour=discord.Colour.blue())
             module_description = ''
             for x in self.bot.cogs:
                 module_description += ('{} - {}'.format(x, self.bot.cogs[x].__doc__) + '\n')
             content.add_field(name='Modules', value=module_description[0:len(module_description) - 1], inline=False)
-            command_description = ''
-            for y in self.bot.commands:
-                if not y.cog_name and not y.hidden:
-                    command_description += ('{} - {}'.format(y.name, y.help) + '\n')
-            content.add_field(name='Commands', value=command_description[0:len(command_description) - 1], inline=False)
+            # command_description = ''
+            # for y in self.bot.commands:
+            #     command_description += ('{} - {}'.format(y.name, y.help) + '\n')
+            # content.add_field(name='Commands', value=command_description[0:len(command_description) - 1], inline=False)
 
             await context.send(embed=content)
+        else:
+            if len(module) > 1:
+                content = discord.Embed(title='Invalid arguments.',
+                                        description='Please only request for one module at once.',
+                                        colour=discord.Colour.red())
+                await context.send(embed=content)
 
 
 def setup(bot):
