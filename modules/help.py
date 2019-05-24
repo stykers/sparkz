2889bce3a1bc9458e53dcc03cf4fba35adcea7fd
Stykers <stykers@stykers.moe>
2019-05-24 16:09:04 +0900

    weird problem but commit anyways

--- modules/help.py
+++ modules/help.py
@@ -12,7 +12,18 @@ class Help(commands.Cog):
     async def help(self, context, *module):
         """The help command."""
         if not module:
-            content = discord.Embed(title='Overall Help', description='Use `s!help <module>` to view help of a specific module')
+            content = discord.Embed(title='Overall Help',
+                                    description='Use `s!help <module>` to view help of a specific module')
+            module_description = ''
+            for x in self.bot.cogs:
+                module_description += ('{} - {}'.format(x, self.bot.cogs[x].__doc__) + '\n')
+            content.add_field(name='Modules', value=module_description[0:len(module_description) - 1], inline=False)
+            command_description = ''
+            for y in self.bot.commands:
+                if not y.cog_name and not y.hidden:
+                    command_description += ('{} - {}'.format(y.name, y.help) + '\n')
+            content.add_field(name='Commands', value=command_description[0:len(command_description) - 1], inline=False)
+
             await context.send(embed=content)
 
 
