5c10b0383d66fa63d9f337ce95949d6b79239c18
Stykers <stykers@stykers.moe>
2019-05-24 18:26:03 +0900

    slap

--- modules/weeb.py
+++ modules/weeb.py
@@ -7,6 +7,7 @@ from discord.ext import commands
 
 class Weeb(commands.Cog):
     """Weeb commands > <"""
+
     def __init__(self, bot):
         self.bot = bot
         self.session = aiohttp.ClientSession(loop=self.bot.loop)
@@ -81,6 +82,36 @@ class Weeb(commands.Cog):
         image = random.choice(choices)
 
         embed = discord.Embed(description=poke.format(author, mention), colour=discord.Colour(0xba4b5b))
+        embed.set_image(url=image)
+
+        await context.send(embed=embed)
+
+    @commands.command(pass_context=True)
+    async def slap(self, context, member: discord.Member):
+        """Slap a meanie!"""
+        author = context.message.author.mention
+        mention = member.mention
+
+        slap = "**{0} slapped {1}!**"
+
+        choices = ['https://cdn.stykers.moe/img/slap/1.gif', 'https://cdn.stykers.moe/img/slap/2.gif',
+                   'https://cdn.stykers.moe/img/slap/3.gif', 'https://cdn.stykers.moe/img/slap/4.gif',
+                   'https://cdn.stykers.moe/img/slap/5.gif', 'https://cdn.stykers.moe/img/slap/6.gif',
+                   'https://cdn.stykers.moe/img/slap/7.gif', 'https://cdn.stykers.moe/img/slap/8.gif',
+                   'https://cdn.stykers.moe/img/slap/9.gif', 'https://cdn.stykers.moe/img/slap/10.gif',
+                   'https://cdn.stykers.moe/img/slap/11.gif', 'https://cdn.stykers.moe/img/slap/12.gif',
+                   'https://cdn.stykers.moe/img/slap/13.gif', 'https://cdn.stykers.moe/img/slap/14.gif',
+                   'https://cdn.stykers.moe/img/slap/15.gif', 'https://cdn.stykers.moe/img/slap/16.gif',
+                   'https://cdn.stykers.moe/img/slap/17.gif', 'https://cdn.stykers.moe/img/slap/18.gif',
+                   'https://cdn.stykers.moe/img/slap/19.gif', 'https://cdn.stykers.moe/img/slap/20.gif',
+                   'https://cdn.stykers.moe/img/slap/21.gif', 'https://cdn.stykers.moe/img/slap/22.gif',
+                   'https://cdn.stykers.moe/img/slap/23.gif', 'https://cdn.stykers.moe/img/slap/24.gif',
+                   'https://cdn.stykers.moe/img/slap/25.gif', 'https://cdn.stykers.moe/img/slap/26.gif',
+                   'https://cdn.stykers.moe/img/slap/27.gif']
+
+        image = random.choice(choices)
+
+        embed = discord.Embed(description=slap.format(author, mention), colour=discord.Colour(0xba4b5b))
         embed.set_image(url=image)
 
         await context.send(embed=embed)
