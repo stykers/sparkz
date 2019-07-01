17e38170cffc1a59acf23e8fa71f44e608fc7d87
Stykers <stykers@stykers.moe>
2019-07-01 14:21:51 +0900

    extract info class

--- util/music.py
+++ util/music.py
@@ -5,3 +5,10 @@ from util import list
 ytdl_npl = YoutubeDL(list.ytdl_noplaylist)
 ytdl = YoutubeDL(list.ytdl_format_options)
 ytdl_aria = YoutubeDL(list.ytdl_aria)
+
+
+def exinfo(url, playlist=False):
+    if playlist:
+        return ytdl.extract_info(url, download=False)
+    else:
+        return ytdl_npl.extract_info(url, download=False)
